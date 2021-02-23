42340889d012d91a64694bd412a062c526dffc0c
smukesh <sharma.mukesh5101@gmail.com>
2021-02-23 20:44:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!!!!!v6!!!!!!!\n'
+  const msg = 'Hello Node!!!!!v7!!!!!!!\n'
   res.end(msg);
 });
 
